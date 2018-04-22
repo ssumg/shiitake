179bded624a458bd94d17597caa7d754e29020b9
ssumg <37644244+ssumg@users.noreply.github.com>
2018-04-22 19:59:25 +0900

    Update shiitake.js

--- shiitake.js
+++ shiitake.js
@@ -5,10 +5,10 @@ function setup() {
 
   createCanvas(800, 800);
 
-  for(int num = 0; num < 5; num++){
+  for(var num = 0; num < 5; num++){
     dog[0][num] = loadImage("L"+num+".png");
   }
-  for(int num = 0; num<5; num++){
+  for(var num = 0; num<5; num++){
     dog[1][num] = loadImage("R"+num+".png");
   }
  
